c9d4f80d0468419b31c7ce7d53a78de2d57534b6
xiaofan.chen <xiaofan.chen@okg.com>
2024-08-07 16:41:01 +0800

    chore: busyloop - 8

--- src/main/java/io/lettuce/bench/AbstractSingleThreadAsync.java
+++ src/main/java/io/lettuce/bench/AbstractSingleThreadAsync.java
@@ -59,7 +59,7 @@ abstract class AbstractSingleThreadAsync<T> {
                     .timeoutOptions(TimeoutOptions.builder().fixedTimeout(Duration.ofSeconds(7200)).build());
             if (useBatchFlush) {
                 optsBuilder.autoBatchFlushOptions(AutoBatchFlushOptions.builder().enableAutoBatchFlush(true).busyLoop(true)
-                        .busyLoopDelayInNanos(100).batchSize(32).build());
+                        .busyLoopDelayInNanos(10).batchSize(8).build());
             }
             redisClient.setOptions(optsBuilder.build());
             final StatefulRedisConnection<byte[], byte[]> connection = redisClient.connect(ByteArrayCodec.INSTANCE);
